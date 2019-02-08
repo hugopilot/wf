2fdea00f783ce9e7a2055df2ff1aa143c3e47f07
Hugo Woesthuis <wijfjes@outlook.com>
2019-02-08 17:06:10 +0100

    Args parsing done...pretty much

--- main.cpp
+++ main.cpp
@@ -7,12 +7,23 @@
 #include <string>
 #include "rmvlib.h"
 
+#define WIPE_ROUNDS 3
+
+void print_intro() {
+	std::cout << "FWIPE V0.1!\n";
+}
 
 int main(int argc, char** argv){
-	std::cout << "FWIPE V0.1!\n";
+	
 	if(argc <=1){ std::cout<<"No Args!\n"; return 1; }
-	std::string fn = argv[1];
-	std::cout << "Deleting " << fn << std::endl;
-	fwipe wiper;
-	wiper.wipe_file(fn.c_str(), 3);
+	std::string fn;
+	for (int i = 1; i < argc; i++) {
+		fn = argv[i];
+		std::cout << "Deleting " << fn << std::endl;
+		fwipe wiper;
+		wiper.wipe_file(fn.c_str(), WIPE_ROUNDS);
+	}
+	
+	std::cout << "\n\nWe are done here!\n";
+	return 0;
 }
